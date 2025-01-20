10388ace162d9439ddc06394609f4d583141b414
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-20 09:15:03 +0200

    add GraphQL configuration and resource definitions for dashboard, companies, and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Refine } from "@refinedev/core";
+import { Authenticated, Refine } from "@refinedev/core";
 import { DevtoolsPanel } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -7,13 +7,16 @@ import "@refinedev/antd/dist/reset.css";
 
 import { dataProvider, liveProvider } from "./Providers";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/layout/index";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import routerBindings, {
   UnsavedChangesNotifier,
   DocumentTitleHandler,
+  CatchAllNavigate,
 } from "@refinedev/react-router";
 import { authProvider } from "./Providers/auth";
 import { Home, Register, ForgotPassword, Login } from "./pages";
+import { resources } from "./config/resources";
 function App() {
   return (
     <BrowserRouter>
@@ -25,6 +28,7 @@ function App() {
             notificationProvider={useNotificationProvider}
             routerProvider={routerBindings}
             authProvider={authProvider}
+            resources={resources}
             options={{
               syncWithLocation: true,
               warnWhenUnsavedChanges: true,
@@ -34,10 +38,23 @@ function App() {
             }}
           >
             <Routes>
-              <Route index element={<Home />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route
+                element={
+                  <Authenticated
+                    key="authenticated-layout"
+                    fallback={<CatchAllNavigate to="/login" />}
+                  >
+                    <Layout>
+                      <Outlet />
+                    </Layout>
+                  </Authenticated>
+                }
+              >
+                <Route index element={<Home />} />
+              </Route>
             </Routes>
             <RefineKbar />
             <UnsavedChangesNotifier />
