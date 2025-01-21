97a14ca6de721c9ce580978fb75f9ceb313db1ba
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-21 03:26:18 +0200

    add UpcomingEvents and DealsChart components to Home page layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,15 @@
+import { DealsChart, UpcomingEvents } from "@/components";
+import { Col, Row } from "antd";
+
 export const Home = () => {
-  return <div>index</div>;
+  return (
+    <Row>
+      <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+        <UpcomingEvents />
+      </Col>
+      <Col xs={24} sm={24} xl={8} style={{ height: "460px" }}>
+        <DealsChart />
+      </Col>
+    </Row>
+  );
 };
