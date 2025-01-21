87a37053de1996bdb91d36d8504ccd3e9eae3b07
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-21 03:26:28 +0200

    add index file to export components for DealsChart, UpcomingEvents, and skeletons

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import DealsChart from "./home/deals-chart";
+import UpcomingEvents from "./home/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import KanabanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+export {
+  DealsChart,
+  UpcomingEvents,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanabanColumnSkeleton,
+  ProjectCardSkeleton,
+  LatestActivitiesSkeleton,
+};
